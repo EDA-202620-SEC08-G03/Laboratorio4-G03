6c836e58fc4e3edeb3aa1639803e4d4e0d72ec0f
juanandresospina1633 <juanandresospina1633@gmail.com>
2026-08-31 12:15:26 -0500

    cambios queue

--- DataStructures/Queue/queue.py
+++ DataStructures/Queue/queue.py
@@ -2,22 +2,26 @@ from DataStructures.List import single_linked_list as sll
 def new_queue():
     queue = sll.new_list()
     return queue
+
 def enqueue(queue,element):
-    sll.addlast(queue,element)
+    sll.add_last(queue,element)
     return queue
+
 def peek (my_queue):
     
     if my_queue["size"] == 0:
         raise Exception('EmptyStructureError: queue is empty')
     
-    primer = my_queue["elements"][0]
+    primer = my_queue["first"]
     return primer
+
 
 def size (my_queue):
     
     return my_queue["size"]
+
 def dequeue(my_queue):
-    x = sll.delete_element(my_queue,0)
+    x = sll.delete_element(my_queue,1)
     return x
 
 def is_empty(my_queue):
